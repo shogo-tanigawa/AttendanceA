09bce72fdcba24d58215e6ea68cc2799528b52da
Shogo <kurohamu2525@gmail.com>
2022-07-31 14:14:57 +0000

    7章7.1終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,6 +10,12 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  def forget(user)
+    user.forget # Userモデル参照
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
     session.delete(:user_id)
     @current_user = nil
