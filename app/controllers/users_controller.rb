0f78727ab48131f55101e996a853e6c8d332aacd
Shogo <kurohamu2525@gmail.com>
2022-08-01 13:36:32 +0000

    8章8.3終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,12 +45,13 @@ class UsersController < ApplicationController
     
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
     end
     
     def correct_user
-      redirect_to(root_url) unless current_user?(user)
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
