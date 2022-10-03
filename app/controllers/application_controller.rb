e5d1d1d504fc0148b9246f725ffa56b376f200d3
shogo <kurohamu2525@gmail.com>
2022-10-03 23:07:08 +0900

    管理者のページ遷移に制限付け完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,6 +29,11 @@ class ApplicationController < ActionController::Base
   def admin_user
     redirect_to root_url unless current_user.admin?
   end
+  
+  # 管理者以外のユーザーであることを確認。
+  def other_user
+    redirect_to root_url if current_user.admin?
+  end
 
   # ページ出力前に1ヶ月分のデータの存在を確認・セットします。
   def set_one_month 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:show, :edit, :update]
   before_action :admin_or_correct_user, only: [:show, :edit, :update]
   before_action :admin_user, only: [:index, :attendance_user_index, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :other_user, only: :show
   before_action :set_one_month, only: :show
   
   def index
